eb1c2a4337b265fc5515c35c9a2bf0ba2c2854f7
Hasan Maki <42813480+nekadcoding@users.noreply.github.com>
2025-01-19 17:11:21 +0700

    Add GraphStream dependencies and configure Maven Shade plugin; simplify main application logic

--- src/main/java/com/coffeecode/App.java
+++ src/main/java/com/coffeecode/App.java
@@ -1,28 +1,8 @@
 package com.coffeecode;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class App {
 
-    private static final Logger logger = LoggerFactory.getLogger(App.class);
-
     public static void main(String[] args) {
-        // Test different log levels
-        logger.debug("This is a debug message");
-        logger.info("This is an info message");
-        logger.warn("This is a warning message");
-        logger.error("This is an error message");
-
-        // Test exception logging
-        try {
-            throw new RuntimeException("Test exception");
-        } catch (RuntimeException e) {
-            logger.error("An error occurred", e);
-        }
-
-        // Test with formatted message
-        String name = "Smart Route";
-        logger.info("Application {} is running", name);
+        System.out.println("Just beginning journey");
     }
 }
